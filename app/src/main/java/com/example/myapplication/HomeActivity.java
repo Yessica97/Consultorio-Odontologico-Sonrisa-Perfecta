efed2dd42b08b15b19381fffc4d8b3abb4797ad8
Yessica97 <yessica.carillo@udea.edu.co>
2022-03-06 15:02:10 -0500

    Se agrega pantalla de home sin redireccionamiento.

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void pantallaRegistro(View view) {
+        Intent intent=new Intent(HomeActivity.this, RegistroActivity.class);
+        startActivity(intent);
+    }
+
+    public void pantallaLista(View view) {
+        Intent intent=new Intent(HomeActivity.this, RegistroActivity.class);
+        startActivity(intent);
+    }
+
+    public void pantallaAgendar(View view) {
+        Intent intent=new Intent(HomeActivity.this, RegistroActivity.class);
+        startActivity(intent);
+    }
+
+    public void pantallaLogin(View view) {
+        Intent intent=new Intent(HomeActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
